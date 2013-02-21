ea9fac8a33cea8a4459fd5e8bd6e949be75903d0
bitey <bitey@outlook.com>
2013-02-20 22:53:11 -0600

    Initial Commit

---
+++ photoAppThatOnlyShowsCats/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  photoAppThatOnlyShowsCats
+//
+//  Created by StopBitingMe on 2/20/13.
+//  Copyright (c) 2013 StopBitingMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ photoAppThatOnlyShowsCats/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  photoAppThatOnlyShowsCats
+//
+//  Created by StopBitingMe on 2/20/13.
+//  Copyright (c) 2013 StopBitingMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
